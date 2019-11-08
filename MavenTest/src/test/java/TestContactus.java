8a1e22dad102424578847afdf22281708195200e
Nishi Agrwal <Nishi.Agrwal@diasparkindia.com>
2019-11-08 12:02:12 +0530

    test

---
+++ MavenTest/src/test/java/TestContactus.java
@@ -0,0 +1,85 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.*;
+
+public class TestContactus {
+
+    String driverPathSet= "C:\\driver\\chromedriver.exe";
+    public String webUrl="https://www.diaspark.com/contact-us/";
+    public WebDriver driver;
+
+    @BeforeClass
+    public void StartWeb(){
+
+        System.setProperty("webdriver.chrome.driver",driverPathSet);
+        driver =new ChromeDriver();
+        Reporter.log("Browser Chrome hit");
+        driver.get(webUrl);
+        Reporter.log("Website is opened now");
+    }
+
+
+    @Test(priority = 1)
+    public  void NameAdd()throws InterruptedException{
+
+        String title= "Contact Us | Diaspark";
+        Assert.assertEquals(driver.getTitle(),title,"assert error message test");
+
+        Thread.sleep(2000);
+        System.out.println("asssert check");
+        //locate the name field in webelement via name tag
+        WebElement cname = driver.findElement(By.name("text-109"));
+
+        //enter text into cname via sendKeys
+        cname.sendKeys("dummydataset");
+    }
+
+    @Test(priority = 2)
+    public  void CompanyAdd(){
+        //locate the company name field in webelement via name tag
+        WebElement com_name = driver.findElement(By.name("text-539"));
+
+        //enter text into company name via sendKeys
+        com_name.sendKeys("testingpurposeonly");
+
+    }
+    @Test(priority = 3)
+    public  void PhoneAdd(){
+        //locate the phone number field via Xpath selector and also allocate value into it
+        driver.findElement(By.xpath("//input[@name='tel-314']")).sendKeys("1234567899");
+
+
+    }
+    @Test(priority = 4)
+    public  void EmailAdd(){
+        //locate the email field via Xpath selector and also allocate value into it
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("nishi.agarwal@diaspark.com");
+    }
+    @Test(priority = 5)
+    public  void MessageAdd(){
+        //locate the message field via tag selector (placeholder) and also allocate value into it
+        driver.findElement(By.name("textarea-116")).sendKeys("it is just use for testing task");
+    }
+    @Test(priority = 6)
+    public  void SendClick() throws  InterruptedException{
+        //locating the send button in webElement via xpath
+        WebElement sendButton = driver.findElement(By.xpath("//input[@value='Send']"));
+
+        //click event on send button
+        sendButton.click();
+
+        //waiting for website to be closed
+
+        Thread.sleep(12000);
+    }
+
+
+    @AfterClass
+    public void CloseWeb(){
+        driver.close();
+    }
+}
---
+++ Selenium_Jars_Test/src/TestSelenium.java
@@ -0,0 +1,73 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestSelenium {
+
+    public String baseUrl = "https://www.diaspark.com/contact-us/";
+    String driverPath = "C:\\driver\\chromedriver.exe";
+    public WebDriver driver;
+
+         @BeforeTest
+         public void LaunchBrowserAndWeb() {
+                //set the property for webdriver consideration
+                //System.setProperty("webdriver.gecko.driver", "C:\\driver\\geckodriver.exe");
+
+                //set the property for webdriver consideration
+                System.setProperty("webdriver.chrome.driver", driverPath);
+
+                //open chrome browser
+                driver = new ChromeDriver();
+
+                //open website
+                driver.get(baseUrl);
+         }
+
+
+         @Test
+         public void MyFirstTest() throws InterruptedException {
+
+             //locate the name field in webelement via name tag
+             WebElement cname = driver.findElement(By.name("text-109"));
+
+             //enter text into cname via sendKeys
+             cname.sendKeys("dummydataset");
+
+             //locate the company name field in webelement via name tag
+             WebElement com_name = driver.findElement(By.name("text-539"));
+
+             //enter text into company name via sendKeys
+             com_name.sendKeys("testingpurposeonly");
+
+             //locate the phone number field via Xpath selector and also allocate value into it
+             driver.findElement(By.xpath("//input[@name='tel-314']")).sendKeys("1234567899");
+
+             //locate the email field via Xpath selector and also allocate value into it
+             driver.findElement(By.xpath("//input[@type='email']")).sendKeys("nishi.agarwal@diaspark.com");
+
+             //locate the message field via tag selector (placeholder) and also allocate value into it
+             driver.findElement(By.name("textarea-116")).sendKeys("it is just use for testing task");
+
+             //locating the send button in webElement via xpath
+             WebElement sendButton = driver.findElement(By.xpath("//input[@value='Send']"));
+
+             //click event on send button
+             sendButton.click();
+
+             //waiting for website to be closed
+
+                 Thread.sleep(20000);
+         }
+
+         @AfterTest
+         public  void CloseBrowser() {
+             //close the webdriver
+             driver.close();
+         }
+    }
+
